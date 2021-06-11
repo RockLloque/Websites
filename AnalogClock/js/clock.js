a73b6ead02a5166a40ef09691834860299b01dcf
rocklloque <thmschmahl@gmail.com>
2021-06-11 15:01:02 +0200

    Initial commit

--- AnalogClock/js/clock.js
+++ AnalogClock/js/clock.js
@@ -2,17 +2,20 @@ const secHand = document.querySelector("#sec-hand");
 const minHand = document.querySelector("#min-hand");
 const hourHand = document.querySelector("#hour-hand");
 
+function rotate(ele, deg) {
+  ele.style.transform = `rotate(${deg + 90}deg)`;
+}
+
 function setDate() {
   const now = new Date();
   const secondsDegrees =
-    now.getSeconds() * 6 + 90; /* 9 = 360 /6 (degrees / seconds per minute) */
-  const minDegrees = now.getMinutes() * 6 + 90;
+    now.getSeconds() * 6; /* 6 = 360 /60 (degrees / seconds per minute) */
+  const minDegrees = now.getMinutes() * 6;
   const hourDegrees =
-    now.getHours() * 30 + 90; /* 30  = 360/12 (degrees / hours per half day) */
-  secHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  minHand.style.transform = `rotate(${minDegrees}deg)`;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  console.log(now.getHours(), now.getMinutes(), now.getSeconds());
+    now.getHours() * 30; /* 30  = 360/12 (degrees / hours per half day) */
+  rotate(secHand, secondsDegrees);
+  rotate(minHand, minDegrees);
+  rotate(hourHand, hourDegrees);
 }
 
 setInterval(setDate, 1000);
